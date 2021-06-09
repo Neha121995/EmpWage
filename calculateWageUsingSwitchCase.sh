b58cc82a5d45e8aba289e1bfa19185bd5e358f57
Neha121995 <jagtapneha1212@gmail.com>
2021-06-09 14:14:41 +0530

    Addre: Calculate Employee Wage using Switch Case

---
+++ calculateWageUsingSwitchCase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+echo "Welcome to Employee Wage Computation Program"
+
+
+isPartTime=1;
+isFullTime=2;
+emprateperHr=20;
+randomcheck=$((RANDOM%3))
+case $randomcheck in
+        $isFullTime)
+                empHrs=8;;
+        $isPartTime)
+                empHrs=4;;
+        *)
+                empHrs=0;;
+esac
+
+salary=$(($empHrs*$emprateperHr))
+echo $salary
