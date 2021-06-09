41670ad201c78f4b9e05ca701bc984e22cefe139
Neha121995 <jagtapneha1212@gmail.com>
2021-06-09 13:12:37 +0530

    Added: Calculate Part Time employee wage

---
+++ calculatPartTimeWage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+echo "Welcome to Employee Wage Computation Program"
+
+
+isPartTime=1;
+isFullTime=2;
+emprateperHr=20;
+randomcheck=$((RANDOM%3))
+if [ $isFullTime -eq $randomcheck ]
+then
+        empHrs=8;
+elif [ $isPartTime -eq $randomcheck ]
+then
+        empHrs=4;
+else
+        empHrs=0;
+fi
+salary=$(($empHrs*$emprateperHr))
+echo $salary
